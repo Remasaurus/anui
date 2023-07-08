cd7e92d46658307b6f84ccfcb104d31281df2584
Remasaurus <101597267+Remasaurus@users.noreply.github.com>
2023-07-08 19:30:28 +0200

    Delete build directory

--- build/CMakeFiles/anui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for anui.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Daniel/Dev/Anui
-# Build directory: /home/Daniel/Dev/Anui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
